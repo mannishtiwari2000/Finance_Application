b0f1a38227cf1f4e4c7f13774a52e8d9c94c3dd3
Manish Tiwari <61265561+mannishtiwari2000@users.noreply.github.com>
2021-06-18 16:35:52 +0530

    update

--- app/src/main/java/com/example/finance_application/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/finance_application/ui/home/HomeFragment.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.finance_application.New_member
+import com.example.finance_application.Profile
 import com.example.finance_application.R
 
 class HomeFragment : Fragment() {
@@ -24,6 +26,12 @@ class HomeFragment : Fragment() {
 
         val root = inflater.inflate(R.layout.fragment_home, container, false)
   var Add_mem:ImageButton=root.findViewById(R.id.Add_member)
+
+         var activity_btn:Button=root.findViewById(R.id.Activity)
+
+        activity_btn.setOnClickListener {
+            startActivity(Intent(context,Profile::class.java))
+        }
 
         Add_mem.setOnClickListener {
             startActivity(Intent(context,New_member::class.java))
